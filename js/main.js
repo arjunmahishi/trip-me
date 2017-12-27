08d49c05b931ded890997b129306e99039130709
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-12-27 16:04:58 +0530

    Finalized UI

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+let experiences = [
+	"hydrogen",
+	"helium"
+];
+
 const randomHexColor = () => {
     let n = (Math.random()*0xfffff|0).toString(16);
     return '#' + (n.length !== 6
         ? (Math.random()*0xf|0).toString(16) + n : n);
 }
+
+const openExp = (exp) => location.pathname = `experiences/${exp}.html`;
+
+experiences.map(exp =>{
+	var card = `
+		<div class="exp-card" onclick="openExp('${exp}')">
+            <div class="exp-card-text">${exp}</div>
+        </div>
+	`;
+
+	document.querySelector(".exp-list").innerHTML += card;
+})
 
 setInterval(() =>{
 	document.querySelector(".nav").style.backgroundColor = randomHexColor();
